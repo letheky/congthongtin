e7433e05f33a6c88b04f6bf42cfe3a8e5b975c1d
Lê Thế Kỷ <kylt@aisolutionsjsc.com>
2025-05-07 11:32:54 +0700

    finish hero

--- components/Home/Hero.vue
+++ components/Home/Hero.vue
@@ -4,7 +4,7 @@
       <swiper-container
         ref="containerRef"
         :init="false"
-        class="w-full h-[80vh]"
+        class="w-full h-[80vh] relative"
       >
         <swiper-slide
           v-for="i in 6"
@@ -16,14 +16,26 @@
             class="w-full h-full object-cover"
             :src="`/images/home/hero-image-${i}.webp`"
             :placeholder="5"
-            preload 
+            preload
             alt="Hero"
           />
         </swiper-slide>
       </swiper-container>
     </ClientOnly>
+    <!-- Navigation -->
+    <div
+      class="swiper-button-prev absolute top-1/2 -translate-y-1/2 left-10 w-10 h-10 z-10 bg-gray-400 rounded-full hover:bg-gray-500 hidden md:block "
+    >
+      <Icon name="chevronLeft" class="text-white w-10 h-10" />
+    </div>
+    <div
+      class="swiper-button-next absolute top-1/2 -translate-y-1/2 right-10 w-10 h-10 z-10 bg-gray-400 rounded-full hover:bg-gray-500 hidden md:block "
+    >
+      <Icon name="chevronRight" class="text-white w-10 h-10" />
+    </div>
+
     <article
-      class="absolute top-40 left-48 min-w-fit flex flex-col justify-center gap-5 z-10"
+      class="absolute top-20 left-10 md:top-40 md:left-48  min-w-fit flex flex-col justify-center gap-5 z-10"
     >
       <h2
         class="text-white text-4xl font-semibold border-l-4 border-red-600 !pl-4 tracking-tighter"
@@ -52,20 +64,13 @@ const containerRef = ref(null);
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const swiper = useSwiper(containerRef, {
-  // effect: "creative",
   loop: true,
   autoplay: {
     delay: 5000,
   },
-  creativeEffect: {
-    prev: {
-      shadow: true,
-      translate: [0, 0, -400],
-    },
-    next: {
-      shadow: true,
-      translate: [0, 0, -400],
-    },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   },
 });
 </script>
