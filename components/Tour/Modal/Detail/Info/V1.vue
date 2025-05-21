1cab5fbbef418bc08f70ab797b7d9eae6904ebfa
Lê Thế Kỷ <kylt@aisolutionsjsc.com>
2025-05-21 10:05:40 +0700

    upload unerror files

---
+++ components/Tour/Modal/Detail/Info/V1.vue
@@ -0,0 +1,88 @@
+<!-- eslint-disable vue/html-self-closing -->
+<template>
+  <div v-if="modelValue" class="detail-modal-wrapper">
+    <div class="detail-modal">
+      <div
+        class="detail-modal-body"
+        :style="{ background: 'url(tourBanner) no-repeat center center ' }"
+      >
+        {{ tourContext }}
+      </div>
+      <div class="detail-modal-footer">
+        <hr />
+        <UButton @click="close">Close</UButton>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const { modelValue, tourContext, tourBanner, close } = defineProps({
+  modelValue: {
+    type: Boolean,
+    required: true,
+  },
+  tourContext: {
+    type: String,
+    required: true,
+  },
+  tourBanner: {
+    type: String,
+    required: true,
+  },
+  close: {
+    type: Function,
+    required: true,
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.detail-modal-wrapper {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.1);
+  backdrop-filter: blur(2rem);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  z-index: $priority-high;
+  .detail-modal {
+    width: 60%;
+    height: 60%;
+    background-color: #ffe9d5;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+    position: relative;
+    padding: 2rem 3rem;
+
+    .detail-modal-body {
+      width: 100%;
+      height: 100%;
+    }
+    .detail-modal-footer {
+      display: flex;
+      flex-direction: column;
+      width: 100%;
+      height: 10rem;
+      padding: 0 3rem;
+
+      hr {
+        width: 100%;
+        margin-bottom: 2rem;
+      }
+      .detail-modal-close {
+        width: 3rem;
+        height: 3rem;
+        margin-left: auto;
+      }
+    }
+  }
+}
+</style>
---
+++ components/Tour/Modal/Detail/V1.vue
@@ -0,0 +1,213 @@
+<!-- eslint-disable vue/html-self-closing -->
+<template>
+  <Transition name="mac-window">
+    <div
+      v-if="modelValue"
+      class="detail-modal-wrapper w-full h-full max-h-[100dvh] top-0 left-0 fixed overflow-hidden"
+    >
+      <div
+        class="close-btn flex justify-end items-center !pr-4 cursor-pointer"
+        @click="close"
+      >
+        Close
+      </div>
+      <div class="detail-modal relative">
+        <div
+          class="absolute top-0 left-0 w-full bg-red-900 overflow-auto h-14 z-8 flex items-center justify-end"
+        >
+          <h3
+            class="mr-4 text-2xl font-bold track-tight inline-block text-white z-10"
+          >
+            {{ getTranslation(currentTour, "name") }}
+          </h3>
+        </div>
+        <div
+          class="modal-logo flex items-center w-[20%] h-18 pl-20 py-3 absolute top-0 left-0 z-10"
+          style="background-color: #ffe9d5"
+        >
+          <NuxtImg src="/images/logo.webp" alt="logo" class="w-10 h-10" />
+        </div>
+        <div class="detail-modal-body">
+          <iframe
+            :src="getTranslation(currentTour, 'link')"
+            allow="autoplay; fullscreen; xr-spatial-tracking"
+            class="w-full h-full"
+          />
+        </div>
+        <div class="detail-modal-footer absolute bottom-0 left-0 w-full z-10" />
+        <div
+          class="detail-modal-footer-overlay bg-red-900 absolute bottom-0 right-0 h-13 w-[50%] z-1"
+        />
+        <div
+          class="detail-modal-footer-overlay-menu absolute bottom-0 right-0 h-15 w-[40%] z-10 flex items-center justify-center gap-4"
+        >
+          <UTooltip
+            :content="{
+              side: 'top',
+              sideOffset: 8,
+            }"
+            text="View tour"
+          >
+            <span class="menu-item">
+              <Icon name="house" />
+            </span>
+          </UTooltip>
+          <UTooltip
+            :content="{
+              side: 'top',
+              sideOffset: 8,
+            }"
+            text="View place details"
+          >
+            <span class="menu-item" >
+              <Icon name="info" />
+            </span>
+          </UTooltip>
+          <UTooltip
+            :content="{
+              side: 'top',
+              sideOffset: 8,
+            }"
+            text="View gallary"
+          >
+            <span class="menu-item">
+              <Icon name="image" />
+            </span>
+          </UTooltip>
+          <UTooltip
+            :content="{
+              side: 'top',
+              sideOffset: 8,
+            }"
+            text="View on map"
+          >
+            <span class="menu-item">
+              <Icon name="pin" />
+            </span>
+          </UTooltip>
+          <div style="position: relative; display: inline-block">
+            <UPopover
+              :popper="{ placement: 'top', offset: 8, align: 'center' }"
+            >
+              <span class="menu-item">
+                <Icon name="website" />
+              </span>
+              <template #content>
+                <LanguageSelectorV2 />
+              </template>
+            </UPopover>
+          </div>
+        </div>
+      </div>
+      <!-- <TourModalDetailInfoV1
+        :model-value="isInfoOpen"
+        :tour-banner="currentTour.thumbnail"
+        :tour-context="getTranslation(currentTour, 'content')"
+        :close="closeInfo"
+      /> -->
+    </div>
+  </Transition>
+</template>
+
+<script setup>
+const { getTranslation } = useTranslation();
+const { modelValue, currentTour, close } = defineProps({
+  modelValue: {
+    type: Boolean,
+    required: true,
+  },
+  currentTour: {
+    type: Object,
+    required: true,
+  },
+  close: {
+    type: Function,
+    required: true,
+  },
+});
+
+// const { isOpen: isInfoOpen, close: closeInfo, open: openInfo } = useModal();
+// const handleOpenInfo = () => {
+//   openInfo();
+// };
+
+// Watch the destructured `modelValue` prop
+watch(
+  () => modelValue,
+  (newValue) => {
+    if (newValue) {
+      // Modal is open, add class to body
+      document.body.classList.add("no-scroll");
+    } else {
+      // Modal is closed, remove class from body
+      document.body.classList.remove("no-scroll");
+    }
+  },
+  { immediate: true }
+); // `immediate: true` ensures it runs once on component mount
+</script>
+
+<style lang="scss" scoped>
+$close-btn-height: 1.5rem;
+$modal-footer-height: 0.5rem;
+
+.detail-modal-wrapper {
+  background-color: rgba(0, 0, 0, 0.1);
+  backdrop-filter: blur(2rem);
+  z-index: $priority-high;
+
+  .close-btn {
+    height: 1.5rem;
+  }
+
+  .detail-modal {
+    width: 100%;
+    height: calc(100% - calc($close-btn-height));
+    background-color: #ffe9d5;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+    position: relative;
+    overflow: hidden;
+
+    .modal-logo {
+      clip-path: polygon(0 0, 100% 0, 70% 100%, 0 100%);
+    }
+
+    .detail-modal-body {
+      width: 100%;
+      height: calc(100% - 2rem);
+      margin-top: 2rem;
+    }
+
+    .detail-modal-footer {
+      height: 0.5rem;
+      background-color: #ffe9d5;
+    }
+    .detail-modal-footer-overlay {
+      clip-path: polygon(0 100%, 10% 0, 100% 0, 100% 100%);
+    }
+    .detail-modal-footer-overlay-menu {
+      background-color: #ffe9d5;
+      clip-path: polygon(0 100%, 15% 0, 100% 0, 100% 100%);
+      .menu-item {
+        width: 2.5rem;
+        height: 2.5rem;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        cursor: pointer;
+        transition: all 0.3s ease-in-out;
+        border-radius: 50%;
+        color: #fff;
+        background-color: #82181a;
+        &:hover {
+          background-color: #fff;
+          color: #82181a;
+        }
+      }
+    }
+  }
+}
+</style>
